cd91af94c379f1523af9a6462646ccfa379f8e30
Axel Bendl <axelbendl@gmail.com>
2025-12-08 00:26:28 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://axelbendl.github.io',
-  base: '/portfoliox',
+  base: '/portfolio',
   vite: {
     plugins: [tailwindcss()],
   },
